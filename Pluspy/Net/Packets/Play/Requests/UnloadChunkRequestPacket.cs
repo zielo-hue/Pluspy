3d2ec19a96a0d4b026b8c6aab0da42ca74d1f7e3
kamasu <invalid@dakota>
2020-03-26 17:54:23 -0700

    Add constructor, rename to UnloadChunkRequestPacket, e

---
+++ Pluspy/Net/Packets/Play/Requests/UnloadChunkRequestPacket.cs
@@ -0,0 +1,25 @@
+﻿using Pluspy.Enums;
+
+namespace Pluspy.Net.Packets.Requests
+{
+    public readonly struct UnloadChunkRequestPacket : IRequestPacket
+    {
+        public byte Id => 0x1E;
+        
+        public int X { get; }
+        public int Z { get; }
+        
+        public UnloadChunkRequestPacket(int x, int z)
+        {
+            X = x;
+            Z = z;
+        }
+        
+        public State WriteTo(MinecraftStream stream, State state)
+        {
+            stream.Write(X);
+            stream.Write(Z);
+            return state;
+        }
+    }
+}
